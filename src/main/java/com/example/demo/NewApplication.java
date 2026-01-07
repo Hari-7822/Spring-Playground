7a648e1ebe87116d0c7a4ce068727c1fca5d3467
Hari-7822 <h26058549@gmail.com>
2026-01-08 01:53:08 +0530

    base

--- src/main/java/com/example/demo/NewApplication.java
+++ src/main/java/com/example/demo/NewApplication.java
@@ -1,13 +1,31 @@
 package com.example.demo;
 
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NewApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Check Beans");
+			String[] names = ctx.getBeanDefinitionNames();
+			Arrays.sort(names);
+			for (String bean : names) {
+				System.out.println(bean);
+			}	
+		};
 	}
 
 }
--- src/test/java/com/example/demo/NewApplicationTests.java
+++ src/test/java/com/example/demo/NewApplicationTests.java
@@ -9,5 +9,5 @@ class NewApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
+ 
 }
